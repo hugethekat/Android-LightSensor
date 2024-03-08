b7bcc42a1d55ab1a9bd88a293f2e2dc0d2e8bf91
hugethekat <117039840+hugethekat@users.noreply.github.com>
2024-03-07 18:39:54 -0700

    LightSensor
    sale

---
+++ SensorLuz/app/src/main/java/com/itson/sensorluz/SensorApp.kt
@@ -0,0 +1,7 @@
+package com.itson.sensorluz
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SensorApp: Application()
---
+++ SensorLuz/app/src/main/java/com/itson/sensorluz/ui/theme/Theme.kt
@@ -0,0 +1,52 @@
+package com.itson.sensorluz.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    secondary = Purple700,
+    tertiary = Teal200
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Purple500,
+    secondary = Purple700,
+    tertiary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun SensorGuideTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
